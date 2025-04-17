edec6c1ea2a9f9160e5664af2b7342499096411f
rich546 <richardboansi69@gmail.com>
2025-04-17 20:08:55 +0000

    Solutions_5240101246_2025.py

--- Assignment 3/Solutions_5240101246_2025.py
+++ Assignment 3/Solutions_5240101246_2025.py
@@ -6,6 +6,9 @@ Solutions to assignment 3
 1.Write a Python program to reverse the string "Programming". Print the reversed string.
 Hint: Use string slicing or a loop.
 """
+text = "Programming"
+reversed_text = text[::-1]
+print("Reversed string:", reversed_text)
 
 
 
@@ -13,6 +16,9 @@ Hint: Use string slicing or a loop.
 2.Create a Python program that takes a user’s full name as input and prints the initials in uppercase.
 Example: Input: "john doe", Output: "J.D."
 """
+full_name = input("Enter your full name: ")
+initials = ".".join([name[0].upper() for name in full_name.split()]) + "."
+print(initials)
 
 
 
@@ -20,6 +26,11 @@ Example: Input: "john doe", Output: "J.D."
 3.Write a Python program to check if a given string is a palindrome. A palindrome reads the same forwards
 and backward (e.g., "radar", "level"). Hint: Compare the string with its reverse.
 """
+text = input("Enter a string: ")
+if text == text[::-1]:
+    print("It is a palindrome.")
+else:
+    print("It is not a palindrome.")
 
 
 
@@ -27,10 +38,17 @@ and backward (e.g., "radar", "level"). Hint: Compare the string with its reverse
 4.Create a Python program that asks the user to enter a sentence and counts the number of words in the sentence.
 Hint: Use the split() method to break the string into words.
 """
+sentence = input("Enter a sentence: ")
+word_count = len(sentence.split())
+print("Number of words:", word_count)
 
 
 
 """
 5.Write a Python program to replace all occurrences of "is" with "was" in the string "This is a string and it
 is an example." Print the modified string.
-"""
+"""  
+text = "This is a string and it is an example."
+modified_text = text.replace("is", "was")
+print(modified_text)
+                                                                                                                                                                                                                     
